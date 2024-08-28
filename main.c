6edc7f95affbab21d8b68ce87ffed8fffd2b8af4
GeorgeSamir10 <georgesamir3500@gmail.com>
2024-08-28 19:12:36 +0300

    Task2 Final

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - main entry point for our progra
+ * main - entry point for our program
  * @ac: Argument count to the main
  * @av: pointer to array of argument values
  *
@@ -12,22 +12,29 @@
 
 int main(int ac, char **av)
 {
-	char *in;
+	char *in = NULL, *Token;
 	size_t len = 0;
 	ssize_t input;
+	pid_t pid;
+	int status, i;
+	char **Temp;
 
 	(void)ac, (void)av;
 	in = (char *)malloc(len * sizeof(char));
-	if (in == NULL)
-	{
-		perror("unallcoated buffer");
-		exit(1);
-	}
 
-	printf("type something");
+	/*
+	*if (in == NULL)
+	*{
+	*	perror("unallcoated buffer");
+	*	exit(1);
+	*}
+	*/
+
+	/* printf("type something"); */
 	while (1)
 	{
 		write(STDOUT_FILENO, "shellprompt$", 13);
+
 		input = getline(&in, &len, stdin);
 
 		if (input == -1)
@@ -36,10 +43,41 @@ int main(int ac, char **av)
 			exit(1);
 		}
 
-		printf("%s", in);
+		Token = strtok(in, " \n");
+
+		Temp = malloc(sizeof(char *) * 1024);
+		i = 0;
+		while (Token)
+		{
+			Temp[i] = Token;
+			Token = strtok(NULL, " \n");
+			i++;
+		}
+		Temp[i] = NULL;
+
+		pid = fork();
+
+		if (pid == 0)
+		{
+			if(execve(Temp[0], Temp, NULL) == -1)
+			{
+				perror("Command Not Found");
+				exit(97);
+			}
+		}
+		else if (pid < 0)
+		{
+			/* failed to create the child process */
+			perror("failed");
+			exit(41);
+		}
+		else
+		{
+			/* wait */
+			wait(&status);
+		}
 	}
 
 	free(in);
-	exit(EXIT_SUCCESS);
 	return (0);
 }
