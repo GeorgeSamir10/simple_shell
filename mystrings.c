4284109c2fb827e3c69f6c70cbb69a94b7620157
GeorgeSamir10 <georgesamir3500@gmail.com>
2024-08-29 20:41:06 +0300

    Edit

--- mystrings.c
+++ mystrings.c
@@ -8,5 +8,21 @@
 
 void _myprint(const char *text)
 {
-	write(STDOUT_FILENO, text, strlen(text));
+	write(STDOUT_FILENO, text, _strlen(text));
+}
+
+/**
+* _Strlen - it gives string length
+* @s: pointer to string
+* Return: length of string
+*/
+int _Strlen(char *s)
+{
+	int j = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (j);
 }
