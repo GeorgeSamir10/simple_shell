5a19d33f0845775fd7ead37f2e5bd2b14a01a96c
GeorgeSamir10 <georgesamir3500@gmail.com>
2024-08-30 14:08:50 +0300

    REFACTORING

--- main.h
+++ main.h
@@ -13,15 +13,7 @@
 #include <stddef.h>
 
 
-extern char **environ;
 
-void _prompt(void);
-void _myprint(char *text);
-void _input(char *_mycmd, size_t inSize);
-void _executing(const char *_mycmd);
-char *_getenv(const char *name);
 
- /* int _putchar(char c); */
- /* int _puts(char *str); */
-int _strlen(char *s);
 #endif /* MAIN_H */
+
