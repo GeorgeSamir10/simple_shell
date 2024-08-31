0c42879b49b4f3a323f15d8acd84a8aaee47359b
MarollaVictor <marollavictorsedky@gmail.com>
2024-08-31 05:58:41 +0300

    final

--- _Fbuildin.c
+++ _Fbuildin.c
@@ -24,26 +24,26 @@ void _EXITS(char **arv)
 
 /**
  * STRTOI - converts a string into an integer
- *@s: pointer to a string
+ *@str: pointer to a string
  *Return: the integer
  */
-int STRTOI(char *s)
+int STRTOI(char *str)
 {
 	int i, _int, sign = 1;
 
 	i = 0;
 	_int = 0;
-	while (!((s[i] >= '0') && (s[i] <= '9')) && (s[i] != '\0'))
+	while (!((str[i] >= '0') && (str[i] <= '9')) && (str[i] != '\0'))
 	{
-		if (s[i] == '-')
+		if (str[i] == '-')
 		{
 			sign = sign * (-1);
 		}
 		i++;
 	}
-	while ((s[i] >= '0') && (s[i] <= '9'))
+	while ((str[i] >= '0') && (str[i] <= '9'))
 	{
-		_int = (_int * 10) + (sign * (s[i] - '0'));
+		_int = (_int * 10) + (sign * (str[i] - '0'));
 		i++;
 	}
 	return (_int);
